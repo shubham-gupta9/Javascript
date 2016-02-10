7c0c4bfe0ff4ba6fdc3618127e2c1d1564f319b4
shubham-gupta9 <shubhamgupta9@ufl.edu>
2016-02-10 09:43:15 -0500

    To find the day of the week in Javascript

---
+++ FindDay.js
@@ -0,0 +1,46 @@
+function findDay(myDate) {
+    // Return day for date myDate(MM/DD/YYYY)
+    // Note that myDate contains the date in string format
+    var my = new Date(myDate);
+    var day = my.getDay();
+    switch(day){
+        case 0:
+            console.log("Sunday");
+            break;
+        case 1:
+            console.log("Monday");
+            break;
+        case 2:
+            console.log("Tuesday");
+            break;
+        case 3:
+            console.log("Wednesday");
+            break;
+        case 4:
+            console.log("Thursday");
+            break;
+        case 5:
+            console.log("Friday");
+            break;
+        case 6:
+            console.log("Saturday");
+            break;
+    }
+    
+} 
+
+// tail starts here
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+    var dates = _input.split('\n');
+
+    for (var i = 0; i < dates.length - 1; i++) {
+        findDay(dates[i]);
+    }
+});
