c8114e83225067056a28b8988ee97b85a0ed9f14
shubham-gupta9 <shubhamgupta9@ufl.edu>
2016-02-10 09:12:14 -0500

    To fetch values from object in javascript

---
+++ Object.js
@@ -0,0 +1,30 @@
+function displayInformation() {
+     for(var i =0;i<library.length;i++){
+         if(library[i].readingStatus == true){
+             console.log("Already read '"+library[i].title+"' by "+ library[i].author+"." );
+         }else{
+             console.log("You still need to read '"+library[i].title+"' by "+ library[i].author+"." );
+         }
+     }
+} 
+
+// tail starts here
+var library = [ 
+    {
+        title: 'Bill Gates',
+        author: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        title: 'Steve Jobs',
+        author: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        author: 'Suzanne Collins',
+        readingStatus: false
+    }
+];
+
+displayInformation();
